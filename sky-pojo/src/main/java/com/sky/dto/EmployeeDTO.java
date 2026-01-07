d81f977fcaec4748fa10576768007a361802f431
Kay <beinuela@users.noreply.github.com>
2026-01-07 19:06:49 +0800

    无事发生

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,10 +1,12 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@Api("员工数据传输对象")
 public class EmployeeDTO implements Serializable {
 
     private Long id;
